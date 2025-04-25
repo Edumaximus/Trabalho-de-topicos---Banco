b859e8b8dbcd3c5e5f08ceb8368330142f12d113
gabrielbrga <johndoe@example.com>
2025-04-25 11:44:10 -0300

    ajuste no appdatacontext

--- MySolution/Banco/API/Models/AppDataContext.cs
+++ MySolution/Banco/API/Models/AppDataContext.cs
@@ -5,11 +5,21 @@ using API.Models;
 
 public class AppDataContext : DbContext{
 
-
+    public DbSet<Conta> Contas { get; set; }
+    public DbSet<TipoDaConta> TiposDasContas { get; set; } 
 
 protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=trabalho.db");
     }
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<TipoDaConta>().HasData(
+
+            new TipoDaConta() {Id = 1, Name = "Adulto"},
+            new TipoDaConta() {Id = 2, Name = "Menor de Idade"},
+            
+        );
+     }
 
 }
